86e3d1540c09c74d2d5f2006e3d4074bda686934
Guy Brukhis <guy.br7@gmail.com>
2017-11-09 01:02:21 +0200

    sort and unique categories

--- src/store/reducers/categories.js
+++ src/store/reducers/categories.js
@@ -1,29 +1,37 @@
+import _orderBy from 'lodash/orderBy'
+import _sortedUniqBy from 'lodash/sortedUniqBy'
+
 const defaultState = []
+
+const sort = categories => {
+  return unique(_orderBy(categories, ['title'], ['asc']))
+}
+
+const unique = sortedCategories => {
+  return _sortedUniqBy(sortedCategories, 'id')
+}
 
 const categories = (state = defaultState, action) => {
   switch (action.type) {
     case 'SET_CATEGORIES':
-      return [
-        ...action.categories
-      ]
+      return sort(action.categories)
 
     case 'ADD_CATEGORIES':
-      //TODO ignore existing categories
-      return [
+      return sort([
         ...state,
         ...action.categories.map(c => ({
           ...c,
           isNew: true
         }))
-      ]
+      ])
 
     case 'MARK_CATEGORIES_AS_NOT_NEW':
-      return [
+      return sort([
         ...state.map(c => ({
           ...c,
           isNew: false
         }))
-      ]
+      ])
 
     default:
       return state
