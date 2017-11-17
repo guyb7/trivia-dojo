d914e2a96b36afbe37afff81210ba715ccb8cd70
Guy Brukhis <guy.br7@gmail.com>
2017-11-17 23:36:55 +0200

    submit-quiz - check answers from db

--- src/api/controllers/Quiz/submit.js
+++ src/api/controllers/Quiz/submit.js
@@ -1,21 +1,31 @@
-const mockAnswers = {
-  'ab1234': 'Showbiz',
-  'ab1235': 'Metallica',
-  'ab1236': 'Thriller, Michael Jackson',
-  'ab1237': 'Dire Straits'
+import { query } from '../../Server/DB'
+
+const getAnswers = async questions => {
+  const ids = questions.reduce((acc, q) => {
+    acc.push(q.id.replace(/[^0-9a-f\-]/g, ''))
+    return acc
+  }, [])
+  const idsSql = ids.join("','")
+  const res = await query(`SELECT id, answer FROM questions WHERE id IN ('${idsSql}')`)
+  const dict = res.rows.reduce((acc, q) => {
+    acc[q.id] = q.answer
+    return acc
+  }, {})
+  return dict
 }
 
-export default questions => {
+export default async questions => {
+  const answers = await getAnswers(questions)
   const correctScore = 30
   const wrongScore = 0
 
   let score = 0
   const results = questions.reduce((acc, q) => {
-    score += q.answer === mockAnswers[q.id] ? correctScore : wrongScore
+    score += q.answer === answers[q.id] ? correctScore : wrongScore
     acc[q.id] = {
-      correctAnswer: mockAnswers[q.id],
-      isCorrect: q.answer === mockAnswers[q.id],
-      score: q.answer === mockAnswers[q.id] ? correctScore : wrongScore
+      correctAnswer: answers[q.id],
+      isCorrect: q.answer === answers[q.id],
+      score: q.answer === answers[q.id] ? correctScore : wrongScore
     }
     return acc
   }, {})
--- src/api/routes/postQuiz.js
+++ src/api/routes/postQuiz.js
@@ -1,8 +1,8 @@
 import { submitQuiz } from '../controllers/Quiz/'
 
 export default (req, res) => {
-  const success = () => {
-    const results = submitQuiz(req.body.questions)
+  const success = async () => {
+    const results = await submitQuiz(req.body.questions)
     res.json(results)
   }
   setTimeout(success, 800)
