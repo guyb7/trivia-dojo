6ae841c36dc257bf7553d3f390b2c6cab4c2908f
Guy Brukhis <guy.br7@gmail.com>
2017-11-17 19:36:56 +0200

    store session data in db

--- src/api/Server/Session.js
+++ src/api/Server/Session.js
@@ -1,23 +1,23 @@
 import session from 'express-session'
 import uuid from 'uuid/v4'
-
-// import ConnectRedis from 'connect-redis'
-// import RedisClient from './DB'
-
-// const RedisStore = require('connect-redis')(session)
-
-// const store = RedisClient ? new RedisStore({
-//   client: RedisClient
-// }) : new session.MemoryStore()
+import ConnectPG from 'connect-pg-simple'
+import { pool } from './DB'
 
 const {
+  RAZZLE_SESSION_TABLE = 'session',
   RAZZLE_COOKIE_SECRET = 'defaultsecret',
   RAZZLE_SESSION_KEY = 'session',
   RAZZLE_COOKIE_DAYS_TOEXPIRE = 90
 } = process.env
 
+const pgSession = ConnectPG(session)
+const store = pool ? new pgSession({
+  pool,
+  tableName: RAZZLE_SESSION_TABLE
+}) : new session.MemoryStore()
+
 const sessionConfig = {
-  // store,
+  store,
   name: RAZZLE_SESSION_KEY,
   secret: RAZZLE_COOKIE_SECRET,
   cookie: { maxAge: Number(RAZZLE_COOKIE_DAYS_TOEXPIRE) * 24 * 60 * 60 * 1000 },
@@ -25,8 +25,6 @@ const sessionConfig = {
   saveUninitialized: true,
   genid: req => uuid()
 }
-
-console.log(sessionConfig)
 
 export default app => {
   app.use(session(sessionConfig))
